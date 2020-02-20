c0113111ac5d6ac081369e6ed3c8ba212e773a8f
Daniel <djac0006@student.monash.edu>
2020-02-20 04:07:57 -0500

    Recursive value schema

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { curry, concat } from 'ramda'
 import * as redis from 'redis'
 import { json_to_pairs, key_to_path, mapKeys, path_to_key } from './pure'
-import { key_or_path_schema } from './schemas'
+import { key_or_path_schema, allowable_value_schema } from './schemas'
 import { user_delete, user_get, user_set } from './user'
 
 const connect = (connection_args) => {
@@ -19,8 +19,10 @@ const connect = (connection_args) => {
             return user_get(key_to_path(key), client)
         },
         set: curry((path: string | any [], json) => {
-            const { error } = key_or_path_schema.validate(path);
-            if (error) return Promise.reject(error)        
+            const { path_error } = key_or_path_schema.validate(path);
+            const { json_error } = allowable_value_schema.validate(json);
+            if (path_error) return Promise.reject(path_error)
+            if (json_error) return Promise.reject(json_error)
             return user_set(key_to_path(path), mapKeys(concat(path_to_key(path)))(json_to_pairs(json)), client)
         }),
         delete: (key: string | any []) => {
--- src/schemas.ts
+++ src/schemas.ts
@@ -6,3 +6,15 @@ export const key_or_path_schema = Joi.alternatives().try(
         Joi.string().min(1)
     ).min(1)
 )
+
+export const allowable_value_schema = Joi.alternatives().try(
+    Joi.object().pattern(
+        Joi.string().pattern(/^0$|^[1-9][0-9]*$/, { invert: true }).pattern(/^[a-zA-Z0-9_]*$/), 
+        Joi.link('...')
+    ), 
+    Joi.any().allow(null, undefined),
+    Joi.string(), 
+    Joi.number().allow(Infinity, -Infinity, NaN), 
+    Joi.bool(), 
+    Joi.array().items(Joi.link('...'))
+)
