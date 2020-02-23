4b60130a415e68c3d6db895a17a318fd03dbdf6b
Daniel <djac0006@student.monash.edu>
2020-02-23 18:19:36 -0500

    New pairs delete fix

--- src/redis.ts
+++ src/redis.ts
@@ -1,4 +1,4 @@
-import { compose, concat, isNil, keys, map, toPairs, unnest } from 'ramda'
+import { compose, concat, isNil, keys, map, toPairs, unnest, reduce } from 'ramda'
 import { map_keys, parse, stringify, key_to_path } from './pure'
 
 export const redis_get = (key_list: string[], client: any): Promise<any[]> => {
@@ -8,7 +8,8 @@ export const redis_get = (key_list: string[], client: any): Promise<any[]> => {
 export const redis_delete = async (old_pairs, key_list: string[], client: any): Promise<any> => {
     if (key_list.length === 0) return Promise.resolve('nothing to do')
     const decorated_keys = key_list.map(concat('redibase_'))
-    client.publish('changes', stringify({ old_pairs, new_pairs: key_list.map(key => ({[key]: null})) }))
+    const new_pairs = reduce((acc, val) => ({ ...acc, [val]: null }), {}, key_list)
+    client.publish('changes', stringify({ old_pairs, new_pairs }))
     return client.del(decorated_keys)
 }
 export const redis_set = (old_pairs, new_pairs, client) => {
